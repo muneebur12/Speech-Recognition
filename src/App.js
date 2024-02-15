f67061ad110ab8c78c46020464c45adfcd8d5154
Muneeb Ashiq <muneebashiq88@gmail.com>
2024-02-15 22:10:46 +0500

    Design the Homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import './App.css'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+       <div className='container'>
+        <h2>Speech to Text Converter</h2>
+        <p>A React hook that convert speech from microphone to text and make it available  to your React components.</p>
+
+        <div className='main-content'>
+
+        </div>
+
+        <div className='btn-style'>
+          <button>Copy to Clipboard</button>
+          <button>Start Listening</button>
+          <button>Stop Listening</button>
+        </div>
+       </div>
+    </>
+  )
 }
 
-export default App;
+export default App
